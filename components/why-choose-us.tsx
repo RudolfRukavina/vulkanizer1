e701e8b9164bf28e43cc2816f9cdd0028bb801c8
Tonči Marinac <toncimarinac@gmail.com>
2024-10-22 20:33:09 +0200

    Bigger top margin to why choose us section

--- components/why-choose-us.tsx
+++ components/why-choose-us.tsx
@@ -82,12 +82,13 @@ export default function WhyChooseUs() {
             style={{ border: 0 }}
             loading="lazy"
             allowFullScreen
+            className="z-50"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.2412648750455!2d-73.98823492404069!3d40.74844097138558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c259a9aeb1c6b5%3A0x35b1cfbc89a6097f!2sEmpire%20State%20Building!5e0!3m2!1sen!2sus!4v1690574312720!5m2!1sen!2sus"
           ></iframe>
         </motion.div>
         <div id="why-choose-us-section" className="h-10"></div>
         <motion.div
-          className="grid grid-cols-1 md:grid-cols-3 relative"
+          className="grid grid-cols-1 md:grid-cols-3 relative mt-32"
           variants={staggerContainer}
           initial="hidden"
           whileInView="visible"
